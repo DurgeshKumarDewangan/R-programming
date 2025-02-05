b2675371c66cb9ab7145a4648bab3794a7984cd2
Durgesh Kumar Dewangan <164731284+DurgeshKumarDewangan@users.noreply.github.com>
2025-02-05 10:33:01 +0000

    Expt-1a

--- Practical/Expt-1/1)Basic Mathematical.R
+++ Practical/Expt-1/1)Basic Mathematical.R
@@ -1,1 +1,22 @@
+basic_math_operations <- function(num){
+    cat("No:", num, "\n")
 
+    sqr <- num^2
+    cat("Square:",sqr,"\n" )
+
+    sqr_root <- sqrt(num)
+    cat("Sqrt_root:", sqr_root,"\n")
+    
+    exp <- exp(num)
+    cat("Exponential:",exp,"\n")
+
+    natural_log <- log(num)
+    cat("Natural Log:",natural_log,"\n")
+
+    log10_value <- log10(num)
+    cat("log10_value:", log10_value,"\n")
+    
+    power <- num^3
+    cat("Power:",power,"\n")
+
+}20
---
+++ Practical/Expt-1/Basic.R
@@ -0,0 +1,16 @@
+# Assigning variables
+x <- 10
+y <- 20
+sum <- x + y
+
+
+# Printing output
+print(sum)
+
+# Creating a vector
+v <- c(1, 2, 3, 4, 5)
+
+# Creating a dataframe
+df <- data.frame(Name = c("A", "B", "C"), Age = c(25, 30, 22))
+
+
